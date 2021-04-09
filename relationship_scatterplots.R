374dc16ebf9ad68a1de706441e15101262c5d231
Juan Lamilla <juan.e.lamilla@gmail.com>
2021-04-09 02:13:30 -0300

    Add plot for winrate  / errors  per game

--- relationship_scatterplots.R
+++ relationship_scatterplots.R
@@ -3,26 +3,34 @@ library(tidyverse)
 library(dslabs)
 ds_theme_set()
 
+team_stats <- Teams %>% filter(yearID %in% 1961:2001)
+
 # Scatterplot of the relationship between HRs and wins
-Teams %>% filter(yearID %in% 1961:2001) %>%
+team_stats %>%
   mutate(HR_per_game = HR / G, R_per_game = R / G) %>%
   ggplot(aes(HR_per_game, R_per_game)) + 
   geom_point(alpha = 0.5)
 
 # Scatterplot of the relationship between stolen bases and wins
-Teams %>% filter(yearID %in% 1961:2001) %>%
+team_stats %>%
   mutate(SB_per_game = SB / G, R_per_game = R / G) %>%
   ggplot(aes(SB_per_game, R_per_game)) + 
   geom_point(alpha = 0.5)
 
 # Scatterplot of the relationship between bases on balls and runs
-Teams %>% filter(yearID %in% 1961:2001) %>%
+team_stats %>%
   mutate(BB_per_game = BB / G, R_per_game = R / G) %>%
   ggplot(aes(BB_per_game, R_per_game)) + 
   geom_point(alpha = 0.5)
 
 # Scatterplot of the relationship between at bats and runs
-Teams %>% filter(yearID %in% 1961:2001) %>%
+team_stats %>%
   mutate(AB_per_game = AB / G, R_per_game = R / G) %>%
   ggplot(aes(AB_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+
+# Scatterplot of the relationship between at win rate and number of fielding errors
+team_stats %>%
+  mutate(win_rate = W / G, E_per_game = E / G) %>%
+  ggplot(aes(win_rate, E_per_game)) + 
   geom_point(alpha = 0.5)
