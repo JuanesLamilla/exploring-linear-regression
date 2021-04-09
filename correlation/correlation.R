463b0732599d25684d5cdfb144d8d0601c3669b2
Juan Lamilla <juan.e.lamilla@gmail.com>
2021-04-09 16:16:53 -0300

    Show distribution of sample correlation

--- correlation/correlation.R
+++ correlation/correlation.R
@@ -24,3 +24,28 @@ galton_heights %>%
 # Calculating correlation coefficient
 # rho <- mean(scale(x)*scale(y))
 galton_heights %>% summarize(r = cor(father, son)) %>% pull(r)
+
+# compute sample correlation
+R <- sample_n(galton_heights, 25, replace = TRUE) %>%
+  summarize(r = cor(father, son))
+R
+
+# Monte Carlo simulation to show distribution of sample correlation
+B <- 1000
+N <- 25
+R <- replicate(B, {
+  sample_n(galton_heights, N, replace = TRUE) %>%
+    summarize(r = cor(father, son)) %>%
+    pull(r)
+})
+qplot(R, geom = "histogram", binwidth = 0.05, color = I("black"))
+
+# expected value and standard error
+mean(R)
+sd(R)
+
+# QQ-plot to evaluate whether N is large enough
+data.frame(R) %>%
+  ggplot(aes(sample = R)) +
+  stat_qq() +
+  geom_abline(intercept = mean(R), slope = sqrt((1-mean(R)^2)/(N-2)))
