cc64ea9f80c8986db239ac64520d8e544a360ff5
Juan Lamilla <juan.e.lamilla@gmail.com>
2021-04-09 02:04:07 -0300

    Create scatterplots of various baseball stats

---
+++ relationship_scatterplots.R
@@ -0,0 +1,22 @@
+library(Lahman)
+library(tidyverse)
+library(dslabs)
+ds_theme_set()
+
+# Scatterplot of the relationship between HRs and wins
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(HR_per_game = HR / G, R_per_game = R / G) %>%
+  ggplot(aes(HR_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+
+# Scatterplot of the relationship between stolen bases and wins
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(SB_per_game = SB / G, R_per_game = R / G) %>%
+  ggplot(aes(SB_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+
+# Scatterplot of the relationship between bases on balls and runs
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(BB_per_game = BB / G, R_per_game = R / G) %>%
+  ggplot(aes(BB_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
